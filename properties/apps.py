45e0937baa26ffa55eb8204ff176dd237ea459fc
Imosemi Ehioken Jeremiah <jay@Imosemis-MacBook-Pro.local>
2025-11-02 00:04:03 +0300

    corrected

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,15 @@
+# properties/apps.py
+
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "properties"
+
+    def ready(self):
+        # Import signals so they are registered
+        try:
+            import properties.signals  # noqa: F401
+        except Exception:
+            # swallow import errors during some management commands or tests
+            pass
--- properties/views.py
+++ properties/views.py
@@ -2,7 +2,10 @@
 
 from django.http import JsonResponse
 from .utils import get_all_properties
+from django.views.decorators.cache import cache_page
 
+
+@cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
     queryset = get_all_properties()
     data = list(queryset.values("id", "title", "description", "price", "location", "created_at"))
